a82054f7346c97decc97c2e563bec423ae4bf1f0
luisNovaes <luismagnovaes@gmail.com>
2023-03-03 12:23:50 +0000

    2023-03-03

---
+++ TaskErro.java
@@ -0,0 +1,28 @@
+public class TaskErro {
+    private String message;
+    private int status;
+
+    public TaskErro() {
+    }
+
+    public TaskErro(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
